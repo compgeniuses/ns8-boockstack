c448c7ea091395e22c92f91393b7b93002bdc1f5
Kemboi Elvis <85908528+kemboi22@users.noreply.github.com>
2024-06-02 14:09:16 +0300

    Update build-images.sh change repobase

--- build-images.sh
+++ build-images.sh
@@ -11,7 +11,7 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/compgeniuses}"
+repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="bookstack"
 BOOKSTACK_TAG="24.02.2"
